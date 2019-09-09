320c16c3331b8f0f6e466557b3c78bc2121b48da
Samuel Barbosa <contato@smkbarbosa.eti.br>
2019-09-09 14:20:18 -0300

    finnaly history

--- src/index.js
+++ src/index.js
@@ -12,19 +12,7 @@ function Square(props){
     );
 }
 
-class Board extends React.Component {
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-    
+class Board extends React.Component {    
     renderSquare(i) {
       return (
           <Square
@@ -66,6 +54,22 @@ class Game extends React.Component {
             }],
             xIsNext: true,
         };
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
     }
     
     render() {
@@ -123,30 +127,3 @@ function calculateWinner(squares) {
     }
     return null;
 }
-
-history = [
-    // Antes da primeira jogada
-    {
-        squares: [
-            null, null, null,
-            null, null, null,
-            null, null, null,
-        ]
-    },
-    // depois da primeira jogada
-    {
-        squares: [
-            null, null, null,
-            null, 'X', null,
-            null, null, null,
-        ]
-    },
-    // Depois da segunda jogada
-    {
-        squares: [
-            null, null, null,
-            null, 'X', null,
-            null, null, 'O',
-        ]
-    },
-]
